eaf3d5c86e83d7b77e91107c61a5e2676292b10f
mentosbabapro <tryhackmeyanhesap@gmail.com>
2024-07-02 11:53:21 +0300

    Update a.lua

--- a.lua
+++ a.lua
@@ -1,125 +1,97 @@
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local LocalPlayer = Players.LocalPlayer
-local Mouse = game.Players.LocalPlayer:GetMouse()
-local CamlockState = false
-local Prediction = 0.1768521
-local HorizontalPrediction = 0.111076110
-local VerticalPrediction = 0.11034856
-local XPrediction = 20
-local YPrediction = 20
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-function FindNearestEnemy()
-    local ClosestDistance, ClosestPlayer = math.huge, nil
-    local CenterPosition =
-        Vector2.new(
-        game:GetService("GuiService"):GetScreenResolution().X / 2,
-        game:GetService("GuiService"):GetScreenResolution().Y / 2
-    )
+local Window = Rayfield:CreateWindow({
+    Name = "Mentos Hub",
+    LoadingTitle = "Mentos Hub",
+    LoadingSubtitle = "by mentos",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = nil, -- Create a custom folder for your hub/game
+       FileName = "Big Hub"
+    },
+    Discord = {
+       Enabled = false,
+       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+    KeySystem = false, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "Untitled",
+       Subtitle = "Key System",
+       Note = "No method of obtaining the key is provided",
+       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
+ })
 
-    for _, Player in ipairs(game:GetService("Players"):GetPlayers()) do
-        if Player ~= LocalPlayer then
-            local Character = Player.Character
-            if Character and Character:FindFirstChild("HumanoidRootPart") and Character.Humanoid.Health > 0 then
-                local Position, IsVisibleOnViewport =
-                    game:GetService("Workspace").CurrentCamera:WorldToViewportPoint(Character.HumanoidRootPart.Position)
+ local PlayerTab = Window:CreateTab("Player(Blox Fruits)", 4483362458) -- Title, Image
 
-                if IsVisibleOnViewport then
-                    local Distance = (CenterPosition - Vector2.new(Position.X, Position.Y)).Magnitude
-                    if Distance < ClosestDistance then
-                        ClosestPlayer = Character.HumanoidRootPart
-                        ClosestDistance = Distance
-                    end
-                end
-            end
-        end
-    end
+ local Slider = PlayerTab:CreateSlider({
+    Name = "WalkSpeed",
+    Range = {10, 100},
+    Increment = 1,
+    Suffix = "Speed",
+    CurrentValue = 10,
+    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+    game.Players.LocalPlayer.Character:SetAttribute("SpeedMultiplier", Value)
+    end,
+ })
 
-    return ClosestPlayer
-end
+ local Slider = PlayerTab:CreateSlider({
+    Name = "Dash Length",
+    Range = {10, 1000},
+    Increment = 1,
+    Suffix = "Length",
+    CurrentValue = 10,
+    Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+     game.Players.LocalPlayer.Character:SetAttribute("DashLength", Value)
+    end,
+ })
 
-local enemy = nil
--- Function to aim the camera at the nearest enemy's HumanoidRootPart
-RunService.Heartbeat:Connect(
-    function()
-        if CamlockState == true then
-            if enemy then
-                local camera = workspace.CurrentCamera
-                camera.CFrame = CFrame.new(camera.CFrame.p, enemy.Position + enemy.Velocity * Prediction)
-            end
-        end
-    end
-)
+ local Slider = PlayerTab:CreateSlider({
+    Name = "Jump Height",
+    Range = {10, 500},
+    Increment = 1,
+    Suffix = "Height",
+    CurrentValue = 10,
+    Flag = "Slider3", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+     game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+    end,
+ })
 
-local Vascal = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local UICorner = Instance.new("UICorner")
-local Logo = Instance.new("ImageLabel")
-local TextButton = Instance.new("TextButton")
-local UICorner_2 = Instance.new("UICorner")
+ local AimbotTab = Window:CreateTab("Aimbot(Recomended for FFA because you can aim to your teammates)", 4483362458) -- Title, Image
 
---Properties:
+ local Button = AimbotTab:CreateButton({
+    Name = "Start",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()()
+        local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()
+        Aimbot.Load()
+        ExunysDeveloperAimbot.Load()    
+    end,
+ })
 
-Vascal.Name = "Vascal"
-Vascal.Parent = game.CoreGui
-Vascal.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ local BGPTab = Window:CreateTab("Big Paintball(NOT 2)", 4483362458) -- Title, Image
 
-Frame.Parent = Vascal
-Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.133798108, 0, 0.20107238, 0)
-Frame.Size = UDim2.new(0, 202, 0, 70)
-Frame.Active = true
-Frame.Draggable = true
+ local Button = BGPTab:CreateButton({
+    Name = "Start",
+    Callback = function()
+        getgenv().Enabled = true
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/WetCheezit/Releases/main/Big-Paintball/KillAll.lua"))()  
+    end,
+ })
 
-local function TopContainer()
-    Frame.Position = UDim2.new(0.5, -Frame.AbsoluteSize.X / 2, 0, -Frame.AbsoluteSize.Y / 2)
-end
+ local YanTab = Window:CreateTab("ForYan", 4483362458) -- Title, Image
 
-TopContainer()
-Frame:GetPropertyChangedSignal("AbsoluteSize"):Connect(TopContainer)
+ local Button = BGPTab:CreateButton({
+    Name = "Start",
+    Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/mentosbabapro/a/main/suzelock.lua"))()
 
-UICorner.Parent = Frame
-
-Logo.Name = "Logo"
-Logo.Parent = Frame
-Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Logo.BackgroundTransparency = 5.000
-Logo.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Logo.BorderSizePixel = 0
-Logo.Position = UDim2.new(0.326732665, 0, 0, 0)
-Logo.Size = UDim2.new(0, 70, 0, 70)
-Logo.Image = "rbxassetid://7229442422"
-Logo.ImageTransparency = 0.200
-
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.BackgroundTransparency = 5.000
-TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.0792079195, 0, 0.18571429, 0)
-TextButton.Size = UDim2.new(0, 170, 0, 44)
-TextButton.Font = Enum.Font.SourceSansSemibold
-TextButton.Text = "Toggle CamLock"
-TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.TextScaled = true
-TextButton.TextSize = 11.000
-TextButton.TextWrapped = true
-local state = true
-TextButton.MouseButton1Click:Connect(
-    function()
-        state = not state
-        if not state then
-            TextButton.Text = "Vascal ON"
-            CamlockState = true
-            enemy = FindNearestEnemy()
-        else
-            TextButton.Text = "Vascal OFF"
-            CamlockState = false
-            enemy = nil
-        end
-    end
-)
-
-UICorner_2.Parent = TextButton
+    end,
+ })
